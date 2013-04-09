bdb1aad462309ee4f493bafdd3ebaf937c4da2aa
stellacello@gmail.com <stellacello@gmail.com>
2013-04-09 16:16:21 -0700

    Signed-off-by: stellacello@gmail.com <stellacello@gmail.com>
    1.  Simplified the configuration of the ReportServer deployment.  The dlls only need to go to ReportServer now.
    2.  Fix bug that would check for the valid time range of the SAML Assertion.
    3.  Fix unit tests.
    4.  Added VS 2012 solutions.

---
+++ RS/Reporting/SecurityExtensions/SAML/SAMLExtension/Backup/TestSSO2/SSO.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.UI.HtmlControls;
+using Common.Web;
+using ForeRunner.Reporting.Extensions.SAMLUtils;
+using TestSSO2;
+
+namespace ForeRunner.Reporting.Extensions.SAML
+{
+    public partial class SSO : System.Web.UI.Page {
+        private string GetIssuer()
+        {
+            return ConfigurationManager.AppSettings["ForeRunnerSAMLExtension.IssuerName"];
+        }
+
+        private string GetACSUrl()
+        {
+            return ConfigurationManager.AppSettings["ForeRunnerSAMLExtension.ACSUrl"];
+        }
+
+        protected void Page_Load(object sender, EventArgs e) {
+            string targetUrl =@"http://google.com/Tenant1/";
+            string authority = SAMLHelperBase.GetAuthorityFromUrl(targetUrl);
+            string idpUrl = SAMLHelperBase.GetIDPUrl(authority);
+            // Set Relay State
+            // TODO:  Need to encrypt this thing before sending this off.
+            // Obviously, all these stuff need to be read from the config too.
+            // Need to get the tenant information based on the UrlReferrer.
+            RelayState.Value = //HtmlUtility.UrlEncode(
+                Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(targetUrl));
+            SAMLRequestHelper helper = new SAMLRequestHelper(new TenantInfo(null, new Uri(idpUrl)), new Uri(GetACSUrl()), GetIssuer());
+            helper.IsGZip = false;
+            helper.IsPostBinding = true;
+            // Set SAML Response
+            //string samlRequest =
+            //@"<samlp:AuthnRequest xmlns:samlp=""urn:oasis:names:tc:SAML:2.0:protocol"" xmlns:saml=""urn:oasis:names:tc:SAML:2.0:assertion"" ID=""_ca6fb75447b813cab79686496998a4202509d2aee8"" Version=""2.0"" IssueInstant=""2013-03-06T17:38:31Z"" Destination=""https://halberd.contrivance.org/adfs/ls/auth/integrated/"" AssertionConsumerServiceURL=""https://dragonbeard.contrivance.org/ReportServer/ACS.ashx"""
+            //+ @"ProtocolBinding=""urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"">" + 
+            //@"<samlp:NameIDPolicy Format=""urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"" AllowCreate=""true""/>" +
+            //@"</samlp:AuthnRequest>"; 
+            WIFHelper wifHelper = new WIFHelper();
+            wifHelper.setRequestProperties(new Uri(GetACSUrl()), "ForeRunner", GetIssuer());
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(wifHelper.getSAMLRequest());
+            SAMLRequest.Value =
+            //    System.Convert.ToBase64String(bytes);
+                //wifHelper.getSAMLRequest();
+                //samlRequest;
+                helper.generateSAMLRequest();
+            //Set Form Action
+            this.frmSSO.Action = idpUrl;
+
+            if (!helper.IsPostBinding)
+            {
+                Uri redirectUrl = new Uri(idpUrl
+                + "?SAMLRequest=" + HtmlUtility.UrlEncode(SAMLRequest.Value) + "&RelayState=" + HtmlUtility.UrlEncode(RelayState.Value));
+                HttpContext.Current.Response.Redirect(redirectUrl.ToString());
+                    //idpUrl + "?SAMLRequest=" + HtmlUtility.UrlEncode(SAMLRequest.Value) + "&RelayState=" + HtmlUtility.UrlEncode(RelayState.Value));
+            }
+            // Add OnLoad Logic so that form will be submitted.
+            HtmlGenericControl body = (HtmlGenericControl)this.Page.FindControl("bodySSO");
+            if (body != null) {
+                body.Attributes.Add("onload", "document.forms.frmSSO.submit();");
+            }
+        }
+    }
+}
---
+++ RS/Reporting/SecurityExtensions/SAML/SAMLExtension/Backup/TestSSO2/WIFHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web;
+using System.Xml;
+using System.IO;
+using Microsoft.IdentityModel.Tokens.Saml2;
+
+namespace TestSSO2
+{
+    public class WIFHelper
+    {
+        private System.Reflection.Assembly o = System.Reflection.Assembly.LoadWithPartialName("Microsoft.IdentityServer");
+        private Type samlProtocolSerializerType;
+        private Type authnRequestType;
+        private Type nameIdentifierPolicyType;
+
+        private Object samlProtocolSerializer;
+        private Object authnRequest;
+        private Object nameIdentifierPolicy;
+
+
+        public WIFHelper() 
+        {
+            samlProtocolSerializerType = o.GetType("Microsoft.IdentityServer.Protocols.Saml.SamlProtocolSerializer");
+            authnRequestType = o.GetType("Microsoft.IdentityServer.Protocols.Saml.AuthenticationRequest");
+            nameIdentifierPolicyType = o.GetType("Microsoft.IdentityServer.Protocols.Saml.NameIdentifierPolicy");
+            samlProtocolSerializer = createSamlProtocolSerializer();
+            authnRequest = createAuthenticationRequest();
+            nameIdentifierPolicy = createNameIdentifierPolicy();
+        }
+
+        private Object createInstanceOfType(Type t)
+        {
+            return Activator.CreateInstance(t);
+        }
+
+        private Type getType(String typeName)
+        {
+            return o.GetType(typeName);
+        }
+
+        private Object createSamlProtocolSerializer() 
+        {
+            return createInstanceOfType(samlProtocolSerializerType);
+            
+        }
+
+        private Object createAuthenticationRequest()
+        {
+            return createInstanceOfType(authnRequestType);
+        }
+
+        private Object createNameIdentifierPolicy()
+        {
+            return createInstanceOfType(nameIdentifierPolicyType);
+        }
+
+        public void setRequestProperties(Uri ACSUrl, String providerName, String issuer)
+        {
+            PropertyInfo propInfo = authnRequestType.GetProperty("AssertionConsumerServiceUrl");
+            propInfo.SetValue(authnRequest, ACSUrl, null);
+            propInfo = authnRequestType.GetProperty("ProviderName");
+            propInfo.SetValue(authnRequest, providerName, null);
+
+            propInfo = nameIdentifierPolicyType.GetProperty("AllowCreate");
+            propInfo.SetValue(nameIdentifierPolicy, true, null);
+            propInfo = nameIdentifierPolicyType.GetProperty("Format");
+            propInfo.SetValue(nameIdentifierPolicy, new Uri("urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"), null);
+
+            propInfo = authnRequestType.GetProperty("NameIdentifierPolicy");
+            propInfo.SetValue(authnRequest, nameIdentifierPolicy, null);
+            propInfo = authnRequestType.GetProperty("ProtocolBinding");
+            propInfo.SetValue(authnRequest, new Uri("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"), null);
+            propInfo = authnRequestType.GetProperty("Issuer");
+            propInfo.SetValue(authnRequest, new Saml2NameIdentifier(issuer), null);
+            propInfo = authnRequestType.GetProperty("Destination");
+            propInfo.SetValue(authnRequest, new Uri("https://halberd.contrivance.org/adfs/ls/auth/integrated/"), null);
+        }
+
+        public void writeSamlRequest(XmlWriter writer)
+        {
+            samlProtocolSerializerType.InvokeMember("WriteAuthnRequest",
+                BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, samlProtocolSerializer, new object[] { writer, authnRequest });
+        }
+
+        public string getSAMLRequest()
+        {
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
+                xmlWriterSettings.OmitXmlDeclaration = true;
+
+                using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, xmlWriterSettings))
+                {
+                    writeSamlRequest(xmlWriter);
+                }
+                return stringWriter.ToString();
+            }
+        }
+    }
+}

7ceede7d8ae066fa15ffb73cbe1718daf0de370c
sdwangbaotong <sdwangbaotong@hotmail.com>
2013-05-02 23:19:08 +0800

    Fixed image resize issue

--- RS/Reporting/ReportManager/ReportManagerMVC/RSProxyAPI/Scripts/Models/FRReport.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/RSProxyAPI/Scripts/Models/FRReport.js
@@ -623,7 +623,8 @@ function WriteRichText(RIContext) {
     return RIContext.$HTMLParent;
 }
 function WriteImage(RIContext) {
-    var $NewObj = $("<IMG/>");
+    var $NewObj = new Image();
+
     var Src = RIContext.RS.ReportViewerAPI + "/GetImage/?";
     var Style = "max-height=100%;max-width:100%;" + GetElementsStyle(RIContext.CurrObj.Elements);
 
@@ -641,16 +642,21 @@ function WriteImage(RIContext) {
         RIContext.$HTMLParent.addClass("overflow-hidden");
     }
 
-    $NewObj.attr("src", Src);
-    $NewObj.attr("Style", Style);
-    $NewObj.attr("alt", "Cannot display image");
-    $NewObj.attr("onload", "ResizeImage(this," + sizingType + "," + containerHeight + "," + containerWidth + ");");
+    $NewObj.src = Src;
+    $NewObj.style = Style;
+    $NewObj.alt = "Cannot display image";
+
+    $NewObj.onload = function () {
+        WriteActionImageMapAreas(RIContext, this.width, this.height);
+        ResizeImage(this, sizingType, RIContext.CurrLocation.Height, RIContext.CurrLocation.Width);
+    };
 
     if (RIContext.CurrObj.Elements.SharedElements.Bookmark != undefined) {
         var $node = $("<a/>");
         $node.attr("name", RIContext.CurrObj.Elements.SharedElements.Bookmark);
         RIContext.$HTMLParent.append($node);
     }
+
     RIContext.$HTMLParent.append($NewObj);
     return RIContext.$HTMLParent;
 }
@@ -671,7 +677,7 @@ function WriteChartImage(RIContext) {
     $NewObj.alt = "Cannot display chart image";
     $NewObj.onload = function () {
         WriteActionImageMapAreas(RIContext, this.width, this.height);
-        //ResizeImage(this);
+        ResizeImage(this, 0, "", "");
     };
 
     if (RIContext.CurrObj.Elements.SharedElements.Bookmark != undefined) {
@@ -779,6 +785,8 @@ function ResizeImage(img, sizingType, maxHeight, maxWidth) {
                     $(img).css("width", width * ratio + "mm");                   
                     $(img).css("max-height", maxHeight + "mm");
                     $(img).css("max-width", width * ratio + "mm");
+                    $(img).css("min-height", maxHeight + "mm");
+                    $(img).css("min-width", width * ratio + "mm");
                 }
                 else {
                     ratio = maxWidth / width;
@@ -787,6 +795,8 @@ function ResizeImage(img, sizingType, maxHeight, maxWidth) {
                     $(img).css("height", height * ratio + "mm");
                     $(img).css("max-width", maxWidth + "mm");
                     $(img).css("max-height", height * ratio + "mm");
+                    $(img).css("min-width", maxWidth + "mm");
+                    $(img).css("min-height", height * ratio + "mm");
                 }
             }
             break;

ceccb504f7dcd4ccc556b22e5a84a11ebd938285
Jason Carlson <jasonc@forerunnersw.com>
2013-05-18 12:24:59 -0700

    Rename and removing files that are not needed

---
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Controllers/CatalogItemsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ForeRunner.RSProxy;
+
+namespace ReportManager.Controllers
+{
+    public class CatalogItemsController : ApiController
+    {
+        private string url = ConfigurationManager.AppSettings["ForeRunner.ReportServerWSUrl"];
+        private string accountName = ConfigurationManager.AppSettings["ForeRunner.TestAccount"];
+        private string accountPWD = ConfigurationManager.AppSettings["ForeRunner.TestAccountPWD"];
+        private string domainName = ConfigurationManager.AppSettings["ForeRunner.TestAccountDomain"];
+        
+        private bool useStub = false;
+        // GET api/catalogitem
+        public IEnumerable<CatalogItem> Get(bool isRecursive = false)
+        {
+            RSProxy rs = new RSProxy(url, new Credentials(Credentials.SecurityTypeEnum.Network, accountName, domainName, accountPWD));
+            rs.UseStub = useStub;
+            return rs.ListChildren("/", isRecursive); 
+        }
+
+        // GET api/catalogitem
+        public IEnumerable<CatalogItem> Get(string path, bool isRecursive = false)
+        {
+            RSProxy rs = new RSProxy(url, new Credentials(Credentials.SecurityTypeEnum.Network, accountName, domainName, accountPWD));
+            rs.UseStub = useStub;
+            return rs.ListChildren(path, isRecursive);
+        }
+    }
+}
---
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReportManager.WebApiApplication" Language="C#" %>

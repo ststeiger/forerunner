2e0412925d41010c0a35f9925e3bb08964e191ce
jontot <jontot@msn.com>
2013-06-12 22:25:06 -0700

    - I basically rewrote the toolbar class to enable the addButtons functionality
    - The creation of the default toolbar uses the addButtons so we are dog fooding the implementation
    
    Signed Off By: Jon Totton <jont@forerunnersw.com>

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Scripts/App/router.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Scripts/App/router.js
@@ -95,10 +95,26 @@ var ApplicationRouter = Backbone.Router.extend({
                 //ToolbarHeight: this.toolbarHeight(),
             });
 
-            $('#mainSectionHeader').toolbar({ $reportViewer: $viewer });
+            // Create / render the toolbar
+            var $toolbar = $('#mainSectionHeader');
+            $toolbar.toolbar({ $reportViewer: $viewer });
+            var btnHome = {
+                btnType: 0,
+                selectorClass: 'fr-button-home',
+                imageClass: 'fr-image-home',
+                click: function (e) {
+                    window.location.href = "#";
+                }
+            };
+            $toolbar.toolbar('addButtons', 2, true, [btnHome]);
+
+            // Let the report viewer know the height of the toolbar
             $viewer.reportViewer('option', 'ToolbarHeight', this.toolbarHeight());
+
+            // Create / render the menu pane
             $('#leftPane').toolpane({ $reportViewer: $viewer });
 
+            // Bind the events to the Application Page
             this.appPageView.bindEvents();
         },
 

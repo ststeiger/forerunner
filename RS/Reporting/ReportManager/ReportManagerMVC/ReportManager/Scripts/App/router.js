abbbac8905a5bcc2f556c1b3a08926ab6879ec15
stellacello@gmail.com <stellacello@gmail.com>
2013-06-18 17:04:18 -0700

    Signed-off-by: stellacello@gmail.com <stellacello@gmail.com>
    1.  Refactored the rm toolbar to use toolbase.
    2.  Still need to call the nav to from the enclosing app.

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Scripts/App/router.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Scripts/App/router.js
@@ -72,21 +72,6 @@ var ApplicationRouter = Backbone.Router.extend({
             this.appPageView.transitionHeader(g_App.ReportManagerHeaderView);
             var me = this;
 
-            $('.fr-image-back', $('#mainSectionHeader')).on('click', function (e, data) {
-                me.historyBack();
-            });
-
-
-            $('.fr-button-fav').on("click",
-                function (e) {
-                    me.transitionToFavorite();
-                });
-
-            $('.fr-button-recent').on("click",
-                function (e) {
-                    me.transitionToRecent();
-                });
-
             catalogItemsModel.fetch({
                 success: function (catalogItemsModel, response, options) {
                     me.appPageView.transitionMainSection(appPageModel,

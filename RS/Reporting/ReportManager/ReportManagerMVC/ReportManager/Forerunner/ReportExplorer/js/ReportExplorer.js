d619290fc9aaf2c4bf35a64df4c4b448fc9206d1
stellacello@gmail.com <stellacello@gmail.com>
2013-07-18 10:43:23 -0700

    Signed-off-by: stellacello@gmail.com <stellacello@gmail.com>
    1.  Fixed the scroll position stickiness and the selections.
    2.  Remove Carousel code from ReportExplorer.js

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/ReportExplorer/js/ReportExplorer.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/ReportExplorer/js/ReportExplorer.js
@@ -16,57 +16,7 @@ $(function () {
             $scrollBarOwner: null,
             navigateTo: null
         },
-        _generateListItem: function (catalogItem) {
-            var me = this;
-            
-            var hasParameters = (String(catalogItem.Path).indexOf("Parameter") !== -1) ? 1 : 0;
-            var reportThumbnailPath = me.options.url
-              + "GetThumbnail/?ReportPath=" + catalogItem.Path + "&DefDate=" + catalogItem.ModifiedDate;
-            var $listItem = new $("<li />");
-            $listItem.addClass("fr-report-center");
-            $listItem.addClass("rm-list-container-item");
-            var $caption = new $("<div />");
-            $caption.addClass("fr-report-center");
-            $listItem.append($caption);
-            var $captiontext = new $("<h3 />");
-            $captiontext.addClass("fr-report-centertext");
-            $captiontext.html(catalogItem.Name);
-            $caption.append($captiontext);
-            var imageSrc;
-            //var targetUrl;
-            var $anchor = new $("<a />");
-            var $img = new $("<img />");
-            $img.addClass("catalogitem");
-            $img.addClass("fr-report-center");
-            if (catalogItem.Type === 1) {
-                imageSrc = "../Forerunner/ReportExplorer/images/folder-icon.png";
-            } else {
-                $img.addClass("reportitem");
-
-                //if (hasParameters) {
-                //    imageSrc = "../Forerunner/ReportExplorer/images/Report-icon.png";
-                //} else {
-                    imageSrc = reportThumbnailPath;
-                //}
-            }
-
-            var action = catalogItem.Type === 1 ? "explore" : "browse";
-              
-            $img.attr("src", imageSrc);
-            $img.error( function () {
-                $(this).attr("src", "../Forerunner/ReportExplorer/images/Report-icon.png");
-            });
-            $img.removeAttr("height");
-            $anchor.on("click", function (event) {
-                if (me.options.navigateTo) {
-                    me.options.navigateTo(action, catalogItem.Path);
-                }
-            });
-            $anchor.append($img);
-            $listItem.append($anchor);
-            return $listItem;
-        },
-        _generatePCListItem: function (catalogItem) {
+        _generatePCListItem: function (catalogItem, isSelected) {
             var me = this;
             //var $selectedItem = null;
 
@@ -74,12 +24,12 @@ $(function () {
             var reportThumbnailPath = me.options.url
               + "GetThumbnail/?ReportPath=" + catalogItem.Path + "&DefDate=" + catalogItem.ModifiedDate;
             var $item = new $("<div />");
-            if (catalogItem.Path === me.options.selectedItemPath) {
+            if (isSelected) {
                 $item.addClass("fr-explorer-item-selected");
                 me.$selectedItem = $item;
             }
             $item.addClass("fr-explorer-item");
-             $item.addClass("image-block");
+            $item.addClass("image-block");
             var $caption = new $("<div />");
             $caption.addClass("fr-report-center");
             $item.append($caption);
@@ -125,140 +75,68 @@ $(function () {
             $item.append($anchor);
             return $item;
         },
-        _renderList: function () {
-            var me = this;
-            var catalogItems = me.options.catalogItems;
-            var decodedPath = me.options.selectedItemPath ? decodeURIComponent(me.options.selectedItemPath) : null;
-            me.element.addClass("reportexplorer");
-            var $carouselContainer = $(".sky-carousel-container", me.$Carousel);
-            var $rmListContainer = $(".rm-list-container", me.$RMList);
-            me.rmListItems = new Array(catalogItems.length);
-            for (var i = 0; i < catalogItems.length; i++) {
-                var catalogItem = catalogItems[i];
-                if (decodedPath && decodedPath === decodeURIComponent(catalogItem.Path)) {
-                    me.selectedItem = i;
-                }
-                $carouselContainer.append(me._generateListItem(catalogItem));
-                me.rmListItems[i] = me._generateListItem(catalogItem);
-                $rmListContainer.append(me.rmListItems[i]);
-            }
-            me.$UL = $rmListContainer;
-        },
         _renderPCView: function () {
             var me = this;
+            me.$UL = me.element.find(".fr-report-explorer");
             var catalogItems = me.options.catalogItems;
             var decodedPath = me.options.selectedItemPath ? decodeURIComponent(me.options.selectedItemPath) : null;
             me.rmListItems = new Array(catalogItems.length);
             for (var i = 0; i < catalogItems.length; i++) {
                 var catalogItem = catalogItems[i];
+                var isSelected = false;
                 if (decodedPath && decodedPath === decodeURIComponent(catalogItem.Path)) {
                     me.selectedItem = i;
+                    isSelected = true;
                 }
-                me.rmListItems[i] = me._generatePCListItem(catalogItem);
-                me.element.find(".fr-report-explorer").append(me.rmListItems[i]);
+                me.rmListItems[i] = me._generatePCListItem(catalogItem, isSelected);
+                me.$UL.append(me.rmListItems[i]);
             }
         },
         _render: function () {
             var me = this;
-
-            if (me._mobileUI()) {
-                me.element.html("<div class='sky-carousel'>" +
-                                "<div class='sky-carousel-wrapper'>" +
-                                "<ul class='sky-carousel-container' />" +
-                                "</div>" +
-                                "</div>" +
-                                "<div class='rm-list'>" +
-                                "<ul class='rm-list-container' />" +
+            me.element.html("<div class='fr-report-explorer'>" +
                                 "</div>");
-                me.$Carousel = $(".sky-carousel", me.element);
-                me.$RMList = $(".rm-list", me.element);
-                me._renderList();
-            }
-            else {
-                me.element.html("<div class='fr-report-explorer'>" +
-                                "</div>");
-                me._renderPCView();
-            }
+            me._renderPCView();
             if (me.$selectedItem) {
                 $(window).scrollTop(me.$selectedItem.offset().top - 50);  //This is a hack for now
                 $(window).scrollLeft(me.$selectedItem.offset().left - 20);  //This is a hack for now
             }
         },
-        _mobileUI: function () {
-            return false;
-            if (screen.availWidth < 1224)
-                return true;
-            else
-                return false;
-        },
         initCarousel: function () {
             var me = this;
-            if (!me._mobileUI())
-                return;
-            var carousel = me.$Carousel.carousel({
-                itemWidth: 250,
-                itemHeight: 350,
-                distance: 15,
-                selectedItemDistance: 50,
-                selectedItemZoomFactor: 1,
-                unselectedItemZoomFactor: 0.67,
-                unselectedItemAlpha: 0.6,
-                motionStartDistance: 250,
-                topMargin: 80,
-                gradientStartPoint: 0.35,
-                gradientOverlayColor: "#f5f5f5",
-                gradientOverlaySize: 200,
-                reflectionDistance: 1,
-                reflectionAlpha: 0.35,
-                reflectionVisible: true,
-                reflectionSize: 70,
-                selectByClick: true
-            });
-            me.$C = carousel;
-            if (me.rmListItems.length > 0) {
-                me._selectCarouselItem();
-                me._scrollList();
-            }
+            var isTouch = forerunner.device.isTouch();
+            if (isTouch) {
+                if (me.rmListItems.length > 0) {
+                    me._scrollList();
+                }
 
-            me.$explorer.bind("scrollstop", function (e) {
-                if (!me.$Carousel.is(":visible")) {
+                me.$explorer.bind("scrollstop", function (e) {
                     me._setSelectionFromScroll();
-                    me._selectCarouselItem();
-                }
-            });
+                });
 
-            me.$explorer.bind("selectionAnimationEnd.sc", function (e) {
-                me.selectedItem = me.$C.selectedItem.index();
-            });
-
-            me.$explorer.resize(function () {
-                if (!me.$Carousel.is(":visible")) {
-                    if (me.$explorer.scrollTop() === 0 && me.$UL.is(":visible"))
-                        me._scrollList();
-                }
-            });
+                me.$explorer.resize(function () {
+                    me._scrollList();
+                });
+            }
         },
         _setSelectionFromScroll: function () {
             var me = this;
-            //var ulPosition = me.$UL.position().top;
             var position = me.$explorer.scrollTop();
             var closest = 0;
+            var lastDistance = 0;
             var closestDistance = Math.abs(position - me.rmListItems[0].position().top);
             for (var i = 1; i < me.rmListItems.length; i++) {
                 var distance = Math.abs(position - me.rmListItems[i].position().top);
                 if (distance < closestDistance) {
                     closest = i;
                     closestDistance = distance;
-                } else if (closest !== 0) {
+                } else if (lastDistance != 0 && distance > lastDistance) {
                     // If closetst is no longer 0 and we are no longer approaching the closest break
                     break;
                 }
+                lastDistance = distance;
             }
             me.selectedItem = closest;
-        },
-        _selectCarouselItem: function () {
-            var me = this;
-            me.$C.select(me._getSelectedItem(), 1);
         },
         _scrollList: function () {
             var me = this;
@@ -277,7 +155,6 @@ $(function () {
         },
         _init: function () {
             var me = this;
-            me.$Carousel = null;
             me.$RMList = null;
             me.$UL = null;
             me.rmListItems = null;

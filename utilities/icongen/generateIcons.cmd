d4f8a81d580d38866dd3cf212846d13f8e203f1d
jontot <jontot@msn.com>
2013-07-26 11:34:10 -0700

    - Added an error message to the generateicons.cmd file
    Signed Off By: Jon Totton <jont@forerunnersw.com>

--- utilities/icongen/generateIcons.cmd
+++ utilities/icongen/generateIcons.cmd
@@ -1,11 +1,19 @@
 @echo off
 
+setlocal
+
 set basePath=%~dp0%
 
 set cssFilename=icons24x24.css
 set iconFilename=icons24x24.png
 
 set icongenExe=%basePath%bin\Debug\icongen.exe
+if exist %icongenExe% goto continue
+echo Error - You must build the Debug version of icongen before running this command file
+exit /b 2
+
+:continue
+
 set inputPath=%basePath%images
 set configPath=%basePath%config.xml
 

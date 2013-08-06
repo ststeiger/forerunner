008349fd4ceeed63d0a6babcc62726e465b655fa
stellar <stellar@forerunnersw.com>
2013-08-05 23:38:04 -0700

    Signed-off-by: stellar <stellar@forerunnersw.com>
    1.  Undo a debugging settings that I accidentally checked in.

--- RS/Reporting/ReportViewer/ReportViewer/ReportViewer.cs
+++ RS/Reporting/ReportViewer/ReportViewer/ReportViewer.cs
@@ -65,8 +65,7 @@ namespace Forerunner.SSRS.Viewer
             //Security
             if (this.Credentials == null)
             {
-                rs.Credentials = new NetworkCredential("TestAccount", "TestPWD!", "PUPPERS");
-                //rs.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+                rs.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
             }
             else
             {

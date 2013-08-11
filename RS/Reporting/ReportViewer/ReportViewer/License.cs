0493edb13451e8d1287bbf5aaccb7ba212d8bcec
jont <jont@forerunnersw.com>
2013-08-11 15:40:20 -0700

    - Converted the TimeBomb to use a BinaryFormatter
    - Added the ssr registry subkey
    
    - Signed off by Jon Totton

--- RS/Reporting/ReportViewer/ReportViewer/License.cs
+++ RS/Reporting/ReportViewer/ReportViewer/License.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 using Microsoft.Win32;
 using System.Net;
 using System.Security.Cryptography;
+using System.Web;
 
 namespace Forerunner.SSR
 {
+    [Serializable()]
     [XmlRoot()]
     public class MachineId
     {
@@ -17,13 +20,9 @@ namespace Forerunner.SSR
 
         private MachineId()
         {
-            cpuId = GetCPUId();
             motherBoardId = GetBaseBoardId();
-
             hostName = GetHostName();
             biosId = GetBIOSId();
-            diskId = GetDiskId();
-            videoId = GetVideoId();
             macId = GetMacId();
         }
         public static MachineId CreateCurrentMachineId()
@@ -33,13 +32,49 @@ namespace Forerunner.SSR
         }
         public bool IsSame(MachineId machineId)
         {
-            // TODO
+            int sameCount = 0;
+            if (motherBoardId.CompareTo(machineId.motherBoardId) == 0)
+            {
+                sameCount++;
+            }
+            if (hostName.CompareTo(machineId.hostName) == 0)
+            {
+                sameCount++;
+            }
+            if (biosId.CompareTo(machineId.biosId) == 0)
+            {
+                sameCount++;
+            }
+            if (macId.CompareTo(machineId.macId) == 0)
+            {
+                sameCount++;
+            }
 
-            return true;
+            return sameCount >= 2;
         }
         private static String GetHostName()
         {
             return Dns.GetHostName();
+        }
+        private static string GetBIOSId()
+        {
+            return identifier("Win32_BIOS", "Manufacturer")
+            + identifier("Win32_BIOS", "SMBIOSBIOSVersion")
+            + identifier("Win32_BIOS", "IdentificationCode")
+            + identifier("Win32_BIOS", "SerialNumber")
+            + identifier("Win32_BIOS", "ReleaseDate")
+            + identifier("Win32_BIOS", "Version");
+        }
+        private static string GetBaseBoardId()
+        {
+            return identifier("Win32_BaseBoard", "Model")
+            + identifier("Win32_BaseBoard", "Manufacturer")
+            + identifier("Win32_BaseBoard", "Name")
+            + identifier("Win32_BaseBoard", "SerialNumber");
+        }
+        private static string GetMacId()
+        {
+            return identifier("Win32_NetworkAdapterConfiguration", "MACAddress", "IPEnabled");
         }
         private static string identifier(string wmiClass, string wmiProperty)
         {
@@ -88,88 +123,27 @@ namespace Forerunner.SSR
             }
             return result;
         }
-        private static string GetCPUId()
-        {
-            //Uses first CPU identifier available in order of preference
-            //Don't get all identifiers, as it is very time consuming
-            string retVal = identifier("Win32_Processor", "UniqueId");
-            if (retVal == "") //If no UniqueID, use ProcessorID
-            {
-                retVal = identifier("Win32_Processor", "ProcessorId");
-                if (retVal == "") //If no ProcessorId, use Name
-                {
-                    retVal = identifier("Win32_Processor", "Name");
-                    if (retVal == "") //If no Name, use Manufacturer
-                    {
-                        retVal = identifier("Win32_Processor", "Manufacturer");
-                    }
-                    //Add clock speed for extra security
-                    retVal += identifier("Win32_Processor", "MaxClockSpeed");
-                }
-            }
-            return retVal;
-        }
-        private static string GetBIOSId()
-        {
-            return identifier("Win32_BIOS", "Manufacturer")
-            + identifier("Win32_BIOS", "SMBIOSBIOSVersion")
-            + identifier("Win32_BIOS", "IdentificationCode")
-            + identifier("Win32_BIOS", "SerialNumber")
-            + identifier("Win32_BIOS", "ReleaseDate")
-            + identifier("Win32_BIOS", "Version");
-        }
-        private static string GetDiskId()
-        {
-            return identifier("Win32_DiskDrive", "Model")
-            + identifier("Win32_DiskDrive", "Manufacturer")
-            + identifier("Win32_DiskDrive", "Signature")
-            + identifier("Win32_DiskDrive", "TotalHeads");
-        }
-        private static string GetBaseBoardId()
-        {
-            return identifier("Win32_BaseBoard", "Model")
-            + identifier("Win32_BaseBoard", "Manufacturer")
-            + identifier("Win32_BaseBoard", "Name")
-            + identifier("Win32_BaseBoard", "SerialNumber");
-        }
-        private static string GetVideoId()
-        {
-            return identifier("Win32_VideoController", "DriverVersion")
-            + identifier("Win32_VideoController", "Name");
-        }
-        private static string GetMacId()
-        {
-            return identifier("Win32_NetworkAdapterConfiguration", "MACAddress", "IPEnabled");
-        }
 
         #endregion  // methods
 
         #region data
 
         [XmlElement()]
-        public String cpuId;
+        public String motherBoardId;
+
+        [XmlElement()]
+        public String hostName;
 
         [XmlElement()]
         public String biosId;
 
         [XmlElement()]
-        public String motherBoardId;
-
-        [XmlElement()]
-        public String diskId;
-
-        [XmlElement()]
-        public String videoId;
-
-        [XmlElement()]
         public String macId;
-
-        [XmlElement()]
-        public String hostName;
 
         #endregion data
     }
 
+    [Serializable()]
     [XmlRoot()]
     public class TimeBomb
     {
@@ -187,7 +161,11 @@ namespace Forerunner.SSR
         // Time bomb grace period expressed in days
         private const int trialPeriod = 60;
         private const String forerunnerKey = "Forerunnersw";
-        private const String timeBombName = "ssr-setup";
+        private const String ssrKey = "ssr";
+        private const String timeBombName = "setupdata";
+        private const String machineHashName = "setupkey";
+
+        private const String genericRegistyError = "Setup error - time bomb data not found";
 
         #endregion types and static constants
 
@@ -209,48 +187,65 @@ namespace Forerunner.SSR
         public static void Remove()
         {
             RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true);
-            softwareKey.DeleteSubKey(TimeBomb.forerunnerKey, true);
+            RegistryKey forerunnerKey = softwareKey.OpenSubKey(TimeBomb.forerunnerKey, true);
+            if (forerunnerKey != null)
+            {
+                forerunnerKey.DeleteSubKey(TimeBomb.ssrKey, false);
+            }
+        }
+        public void SaveToRegistry()
+        {
+            // Serialize the time bomb
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter serializer = new BinaryFormatter();
+            serializer.Serialize(stream, this);
+
+            // TODO Create an MD5 Hash from the unencrypted string
+
+            // TODO Encrypt the string into a byte array
+
+            // Save the time bomb to the registry
+            RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true);
+            RegistryKey forerunnerswKey = softwareKey.CreateSubKey(TimeBomb.forerunnerKey);
+            RegistryKey ssrKey = forerunnerswKey.CreateSubKey(TimeBomb.ssrKey);
+            ssrKey.SetValue(TimeBomb.timeBombName, stream.GetBuffer(), RegistryValueKind.Binary);
+            stream.Close();
         }
         public static TimeBomb LoadFromRegistry()
         {
             // Load the time bomb from the registry
             RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey("SOFTWARE");
             RegistryKey forerunnerswKey = softwareKey.OpenSubKey(TimeBomb.forerunnerKey);
-
             if (forerunnerswKey == null)
             {
-                LicenseException e = new LicenseException("Setup error - time bomb data not found");
+                LicenseException e = new LicenseException(TimeBomb.genericRegistyError);
                 e.Data.Add(TimeBomb.failKey, FailReason.TimeBombMissing);
                 throw e;
             }
 
-            String timeBombString = (String)forerunnerswKey.GetValue(TimeBomb.timeBombName);
-
-            if (timeBombString == null)
+            RegistryKey ssrKey = forerunnerswKey.OpenSubKey(TimeBomb.ssrKey);
+            if (ssrKey == null)
             {
-                LicenseException e = new LicenseException("Setup error - time bomb data not found");
+                LicenseException e = new LicenseException(TimeBomb.genericRegistyError);
                 e.Data.Add(TimeBomb.failKey, FailReason.TimeBombMissing);
                 throw e;
             }
+
+            byte[] timeBombData = (byte[])ssrKey.GetValue(TimeBomb.timeBombName);
+
+            if (timeBombData == null)
+            {
+                LicenseException e = new LicenseException(TimeBomb.genericRegistyError);
+                e.Data.Add(TimeBomb.failKey, FailReason.TimeBombMissing);
+                throw e;
+            }
+
+            // TODO Decrypt the time bomb data
 
             // Deserialize the time bomb
-            StringReader reader = new StringReader(timeBombString);
-            XmlSerializer serializer = new XmlSerializer(typeof(TimeBomb));
-            return (TimeBomb)serializer.Deserialize(reader);
-        }
-        public void SaveToRegistry()
-        {
-            // Serialize the time bomb
-            StringBuilder sb = new StringBuilder();
-            StringWriter writer = new StringWriter(sb);
-            XmlSerializer serializer = new XmlSerializer(typeof(TimeBomb));
-            serializer.Serialize(writer, this);
-
-            // Save the time bomb to the registry
-            RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true);
-            RegistryKey forerunnerswKey = softwareKey.CreateSubKey(TimeBomb.forerunnerKey);
-            forerunnerswKey.SetValue(TimeBomb.timeBombName, sb.ToString(), RegistryValueKind.String);
-            writer.Close();
+            MemoryStream stream = new MemoryStream(timeBombData);
+            BinaryFormatter serializer = new BinaryFormatter();
+            return (TimeBomb)serializer.Deserialize(stream);
         }
         public bool IsValid(MachineId currentMachineId)
         {
@@ -272,70 +267,6 @@ namespace Forerunner.SSR
             }
 
             return true;
-        }
-        private static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
-        {
-            byte[] encrypted;
-
-            // Create an AesManaged object 
-            // with the specified key and IV. 
-            using (AesManaged aesAlg = new AesManaged())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption. 
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-
-                            //Write all data to the stream.
-                            swEncrypt.Write(plainText);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-            }
-
-            // Return the encrypted bytes from the memory stream. 
-            return encrypted;
-        }
-        private static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
-        {
-            // Declare the string used to hold the decrypted text. 
-            string plaintext = null;
-
-            // Create an AesManaged object with the specified key and IV.
-            using (AesManaged aesAlg = new AesManaged())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption. 
-                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
-                {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-
-                            // Read the decrypted bytes from the decrypting stream and place them in a string.
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-
-            }
-            return plaintext;
         }
 
         #endregion //methods

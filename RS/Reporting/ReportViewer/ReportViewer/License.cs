58f11e1953d83e71786d3b704e1613a773faba46
jont <jont@forerunnersw.com>
2013-08-12 11:25:04 -0700

    - Added the cryptographic hash value to the TimeBomb as well as the registry
    - Encrypted the timebomb stored in the registry with the MachineKey.protect method.
    
    - Signed off by Jon Totton

--- RS/Reporting/ReportViewer/ReportViewer/License.cs
+++ RS/Reporting/ReportViewer/ReportViewer/License.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Security;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
-using Microsoft.Win32;
-using System.Net;
 using System.Security.Cryptography;
-using System.Web;
+using Microsoft.Win32;
 
 namespace Forerunner.SSR
 {
@@ -164,6 +165,7 @@ namespace Forerunner.SSR
         private const String ssrKey = "ssr";
         private const String timeBombName = "setupdata";
         private const String machineHashName = "setupkey";
+        private const String EncryptPurpose = "K34khron56sTg";
 
         private const String genericRegistyError = "Setup error - time bomb data not found";
 
@@ -177,6 +179,7 @@ namespace Forerunner.SSR
             TimeBomb timeBomb = new TimeBomb();
             timeBomb.start = installDate;
             timeBomb.machineId = MachineId.CreateCurrentMachineId();
+            timeBomb.CreateCryptoHash();
 
             return timeBomb;
         }
@@ -193,22 +196,38 @@ namespace Forerunner.SSR
                 forerunnerKey.DeleteSubKey(TimeBomb.ssrKey, false);
             }
         }
-        public void SaveToRegistry()
+        private MemoryStream Serialize()
         {
             // Serialize the time bomb
             MemoryStream stream = new MemoryStream();
             BinaryFormatter serializer = new BinaryFormatter();
             serializer.Serialize(stream, this);
+            return stream;
+        }
+        private void CreateCryptoHash()
+        {
+            MemoryStream stream = Serialize();
 
-            // TODO Create an MD5 Hash from the unencrypted string
+            // Create an 8 byte, cryptographic hash and convert it to a base 64 string from the unencrypted
+            // serializer stream of this object
+            MD5Cng md5 = new MD5Cng();
+            byte[] machineHash = md5.ComputeHash(stream);
+            cryptoHash = Convert.ToBase64String(machineHash.Take(8).ToArray());
+        }
+        public void SaveToRegistry()
+        {
+            // Serialize the time bomb
+            MemoryStream stream = Serialize();
 
-            // TODO Encrypt the string into a byte array
+            // Encrypt the string into a byte array
+            byte[] timeBombProtected = MachineKey.Protect(stream.GetBuffer(), TimeBomb.EncryptPurpose);
 
             // Save the time bomb to the registry
             RegistryKey softwareKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true);
             RegistryKey forerunnerswKey = softwareKey.CreateSubKey(TimeBomb.forerunnerKey);
             RegistryKey ssrKey = forerunnerswKey.CreateSubKey(TimeBomb.ssrKey);
-            ssrKey.SetValue(TimeBomb.timeBombName, stream.GetBuffer(), RegistryValueKind.Binary);
+            ssrKey.SetValue(TimeBomb.timeBombName, timeBombProtected, RegistryValueKind.Binary);
+            ssrKey.SetValue(TimeBomb.machineHashName, cryptoHash, RegistryValueKind.String);
             stream.Close();
         }
         public static TimeBomb LoadFromRegistry()
@@ -231,16 +250,17 @@ namespace Forerunner.SSR
                 throw e;
             }
 
-            byte[] timeBombData = (byte[])ssrKey.GetValue(TimeBomb.timeBombName);
+            byte[] timeBombProtected = (byte[])ssrKey.GetValue(TimeBomb.timeBombName);
 
-            if (timeBombData == null)
+            if (timeBombProtected == null)
             {
                 LicenseException e = new LicenseException(TimeBomb.genericRegistyError);
                 e.Data.Add(TimeBomb.failKey, FailReason.TimeBombMissing);
                 throw e;
             }
 
-            // TODO Decrypt the time bomb data
+            // Decrypt the time bomb data
+            byte[] timeBombData = MachineKey.Unprotect(timeBombProtected, TimeBomb.EncryptPurpose);
 
             // Deserialize the time bomb
             MemoryStream stream = new MemoryStream(timeBombData);
@@ -274,10 +294,11 @@ namespace Forerunner.SSR
         #region data
 
         [XmlElement()]
-        public DateTime start;         // Time Bomb Start date / time
-
+        public DateTime start;          // Time Bomb Start date / time
         [XmlElement()]
-        public MachineId machineId;    // Machine Id where the Time Bomb was created
+        public MachineId machineId;     // Machine Id where the Time Bomb was created
+        [XmlElement()]
+        public String cryptoHash;       // Cryptographic hash string
 
         #endregion  // data
     }

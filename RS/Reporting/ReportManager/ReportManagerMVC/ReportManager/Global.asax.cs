5cdf4c341cddda8120b26a4c0836e025093aab3a
stellar <stellar@forerunnersw.com>
2013-08-29 03:07:42 -0700

    Signed-off-by: stellar <stellar@forerunnersw.com>
    1.  Add forms auth

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Global.asax.cs
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace ReportManager
 {

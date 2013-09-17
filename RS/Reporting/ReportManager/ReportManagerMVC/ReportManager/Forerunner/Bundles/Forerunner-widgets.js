39639df49785cb102bc1f20d7903c7445cecda92
Jason Carlson <jasonc@forerunnersw.com>
2013-09-17 13:07:34 -0700

    updated viewer.js to coding standard
    Update website to GoDaddy

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -100,7 +100,7 @@ $(function () {
             var isTouch = forerunner.device.isTouch();
             // For touch device, update the header only on scrollstop.
             if (isTouch) {
-                $(window).bind('scrollstop', function () { me._updateTableHeaders(me); });
+                $(window).bind("scrollstop", function () { me._updateTableHeaders(me); });
             } else {
                 $(window).scroll(function () { me._updateTableHeaders(me); });
             }
@@ -307,7 +307,7 @@ $(function () {
 
         allowSwipe: function(isEnabled){
             var me = this;
-            $(me.element).data('swipeEnabled', isEnabled);
+            $(me.element).data("swipeEnabled", isEnabled);
             /*
             if (isEnabled === true)
                 $(me.element).swipe("enable");
@@ -318,33 +318,33 @@ $(function () {
         _touchNav: function () {
             // Touch Events
             var me = this;
-            $(me.element).hammer({}).on('swipe drag touch release',
+            $(me.element).hammer({}).on("swipe drag touch release",
                 function (ev) {
                     if (!ev.gesture) return;
                     switch (ev.type) {
                         // Hide the header on touch
-                        case 'touch':
+                        case "touch":
                             me._hideTableHeaders();
                             break;
                         // Use the swipe and drag events because the swipeleft and swiperight doesn't seem to fire
-                        case 'swipe':
-                        case 'drag':
-                            if ($(me.element).data('swipeEnabled') == false)
+                        case "swipe":
+                        case "drag":
+                            if ($(me.element).data("swipeEnabled") === false)
                                 return;
 
-                            if (ev.gesture['direction'] == 'left' && ev.gesture['velocityX'] > 0.2) {
+                            if (ev.gesture.direction === "left" && ev.gesture.velocityX > 0.2) {
                                 me.navToPage(me.curPage + 1);
                             }
 
-                            if (ev.gesture['direction'] == 'right' && ev.gesture['velocityX'] > 0.2) {
+                            if (ev.gesture.direction === "right" && ev.gesture.velocityX > 0.2) {
                                 me.navToPage(me.curPage - 1);
                             }
                             
                             break;
                         // Show the header on release only if this is not scrolling.
                         // If it is scrolling, we will let scrollstop handle that.
-                        case 'release':
-                            if (ev.gesture['velocityX'] == 0 && ev.gesture['velocityY'] == 0)
+                        case "release":
+                            if (ev.gesture.velocityX === 0 && ev.gesture.velocityY === 0)
                                 me._updateTableHeaders(me);
                             break;
                     }

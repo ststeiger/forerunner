8a1637c7818aeb205f1527d2ac48f9c6a8c9072f
jont <jont@forerunnersw.com>
2013-09-29 17:26:38 -0700

    - Added the new tab so the user can do licensing. This is not end to end complete yet. But is will act as a prototype and promote discussion.
    - Signed off by Jon Totton

---
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportMannagerConfigTool/ConfigTool/frmManualActivation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportMannagerConfigTool
+{
+    public partial class frmManualActivation : Form
+    {
+        public frmManualActivation()
+        {
+            try
+            {
+                InitializeComponent();
+
+                // It is assumed that the file "Manual Activation.rtf" is in the same folder as MobilizerConfigTool.exe
+                richTxtBoxManualActivation.LoadFile("Manual Activation.rtf");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void richTxtBoxManualActivation_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmManualActivation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

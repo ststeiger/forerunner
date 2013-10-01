d7b530091a4aca75c7e375c559446accf10286a9
stellar <stellar@forerunnersw.com>
2013-09-30 18:21:25 -0700

    Signed-off-by: stellar <stellar@forerunnersw.com>
    1.  Add lazy loading to the page nav.
    2.  Check in the code into externals.

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -2138,7 +2138,10 @@ $(function () {
                 $caption.addClass("fr-report-center");
                 var $thumbnail = new $("<IMG />");
                 $thumbnail.addClass("fr-nav-page-thumb");
-                $thumbnail.attr("src", url);
+                // Instead of stating the src, use data-original and add the lazy class so that
+                // we will use lazy loading.
+                $thumbnail.addClass("lazy");
+                $thumbnail.attr("data-original", url);
                 $thumbnail.data("pageNumber", i);
                 this._on($thumbnail, {
                     click: function (event) {
@@ -2175,6 +2178,7 @@ $(function () {
 
 
             var $list = me._renderList();
+            me.$list = $list;
 
             $sliderWrapper.append($list);
             me.element.css("display", "block");
@@ -2203,6 +2207,9 @@ $(function () {
                 me.isRendered = true;
             }
             me._makeVisible(!me.element.is(":visible"));
+
+            $container = $('ul.fr-nav-container', $(me.element));
+            $(".lazy", me.$list).lazyload({ container: $container });
         },
         _initCallbacks: function () {
             var me = this;

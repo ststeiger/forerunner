aabff338af68295ce46cf9e425d7ebc541c668f1
Stella Rowlett <stellacello@gmail.com>
2013-10-05 15:13:11 -0700

    Converting more projects to use version info from the build.

--- RS/Reporting/SecurityExtensions/SAML/SAMLExtension/TestSSO2/Properties/AssemblyInfo.cs
+++ RS/Reporting/SecurityExtensions/SAML/SAMLExtension/TestSSO2/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestSSO2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestSSO2")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +29,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ utilities/SetupUtil/SetupUtil/VersionTemplate.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Reflection;
+ 
+[assembly:AssemblyFileVersion("0.9.0.377")]
+[assembly: AssemblyCompany("Forerunner Software")]
+[assembly: AssemblyProduct("Mobilizer")]
+[assembly: AssemblyCopyright("Copyright © Forerunner Software 2013")]
+

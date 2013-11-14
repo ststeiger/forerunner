59c832354ebe91c4f92a75fdb449d1cc7964a9e5
Stella Rowlett <stellacello@gmail.com>
2013-11-13 22:34:12 -0800

    advance to v1.1

--- build/variables.cmd
+++ build/variables.cmd
@@ -4,6 +4,7 @@ REM Set Project specific variables here.
 REM
 set PATH=%PATH%;%windir%\Microsoft.Net\Framework\v4.0.30319
 set EnableNuGetPackageRestore=true
+set BRANCH=master
 set GITHUBSSH=git@github.com:forerunnersw/Forerunner.git
 set PROJECT_NAME=Forerunner
 set HOME=%HOMEDRIVE%%HOMEPATH%

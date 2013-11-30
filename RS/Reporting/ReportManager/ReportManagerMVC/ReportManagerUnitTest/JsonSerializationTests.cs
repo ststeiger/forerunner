58cd05ea4e8cd92b5158e47ab26df2444c754c78
jont <jont@forerunnersw.com>
2013-11-30 11:28:23 -0800

    - Added a unit test to check how to do Json serialization of a map
    - Signed off by Jon Totton

---
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManagerUnitTest/JsonSerializationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Script.Serialization;
+
+namespace ReportManagerUnitTest
+{
+    [TestClass]
+    public class JsonSerializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string json = "{\"id\":\"13\", \"value\": \"foo\"}";
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var map = serializer.Deserialize<dynamic>(json);
+        }
+    }
+}

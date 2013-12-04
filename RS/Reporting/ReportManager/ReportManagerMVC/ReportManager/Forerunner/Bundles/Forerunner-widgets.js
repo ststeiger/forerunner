e178349616943508f0048ffe5e58ba0a62b70a0b
sdwangbaotong <sdwangbaotong@hotmail.com>
2013-12-04 20:07:13 +0800

    Bug 707 - fix parameter label width issue.
    -push to v1 by baotong

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -6027,7 +6027,6 @@ $(function () {
             if (me._reportDesignError !== null)
                 me._reportDesignError += me.options.$reportViewer.locData.messages.contactAdmin;
 
-            me._resetLabelWidth();
             me.resetValidateMessage();
             $(".fr-param-form", me.$params).validate({
                 errorPlacement: function (error, element) {
@@ -6190,7 +6189,7 @@ $(function () {
         },
         _writeParamControl: function (param, $parent, pageNum) {
             var me = this;
-            var $label = new $("<div class='fr-param-label' style='width:100%;'>" + param.Prompt + "</div>");
+            var $label = new $("<div class='fr-param-label'>" + param.Prompt + "</div>");
             var bindingEnter = true;
             var dependenceDisable = me._checkDependencies(param);
 
@@ -6788,15 +6787,6 @@ $(function () {
                 return null;
             else
                 return param.value;
-        },
-        _resetLabelWidth: function () {
-            var max = 0;
-            $(".fr-param-label", this.$params).each(function (index, obj) {
-                if ($(obj).width() > max) max = $(obj).width();
-            });
-            $(".fr-param-label", this.$params).each(function (index, obj) {
-                $(obj).width(max);
-            });
         },
         /**
         * @function $.forerunner.reportParameter#resetValidateMessage

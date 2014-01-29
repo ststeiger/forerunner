dfd6608362441196858d971811c11e7b6b8522a9
stellar <stellar@forerunnersw.com>
2014-01-28 23:07:51 -0800

    Signed-off-by: stellar <stellar@forerunnersw.com>
    1.  Fix issue 823, 816, 817, 818, and 819.

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -7273,8 +7273,29 @@ $(function () {
                 me._closeAllDropdown();
             }
         },
+        _shouldInclude: function (param, noValid) {
+            if (!noValid) return true;
+            var me = this;
+            
+            var isString = $(param).attr("datatype") === "String";
+            var allowBlank = $(param).attr("allowblank") === "true";
+
+            // If it is a string type
+            if (isString && allowBlank) return true;
+
+            if (param.value == "") {
+                return me._isNullChecked(param);
+            }
+
+            var required = !!$(param).attr("required");
+            if (required && param.value === me.options.$reportViewer.locData.paramPane.required) {
+                return false;
+            }
+
+            return true;
+        },
         /**
-         * @function $.forerunner.reportParameter#getParamList
+         * @function $.forerunner.reportParameter#getParamsList
          * @generate parameter list base on the user input and return
          */
         getParamsList: function (noValid) {
@@ -7284,20 +7305,24 @@ $(function () {
                 var a = [];
                 //Text
                 $(".fr-param", me.$params).filter(":text").each(function () {
-                    if ($(this).attr("ismultiple") === "false") {
-                        a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: me._isParamNullable(this) });
-                    } else {
-                        var jsonValues = $(this).attr("jsonValues");
-                        a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: JSON.parse(jsonValues ? jsonValues : null) });
+                    if (me._shouldInclude(this, noValid)) {
+                        if ($(this).attr("ismultiple") === "false") {
+                            a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: me._isParamNullable(this) });
+                        } else {
+                            var jsonValues = $(this).attr("jsonValues");
+                            a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: JSON.parse(jsonValues ? jsonValues : null) });
+                        }
                     }
                 });
                 //Hidden
                 $(".fr-param", me.$params).filter("[type='hidden']").each(function () {
-                    if ($(this).attr("ismultiple") === "false") {
-                        a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: me._isParamNullable(this) });
-                    } else {
-                        var value = me._isParamNullable(this);
-                        a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: JSON.parse(value ? value : null) });
+                    if (me._shouldInclude(this, noValid)) {
+                        if ($(this).attr("ismultiple") === "false") {
+                            a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: me._isParamNullable(this) });
+                        } else {
+                            var value = me._isParamNullable(this);
+                            a.push({ Parameter: this.name, IsMultiple: $(this).attr("ismultiple"), Type: $(this).attr("datatype"), Value: JSON.parse(value ? value : null) });
+                        }
                     }
                 });
                 //dropdown
@@ -7353,22 +7378,26 @@ $(function () {
                 return null;
             }
         },
+        _isNullChecked: function (param) {
+            var $cb = $(".fr-param-checkbox", this.$params).filter("[name*='" + param.name + "']").first();
+            return $cb.length !== 0 && $cb.attr("checked") === "checked";
+        },
         _isParamNullable: function (param) {
-            var $cb = $(".fr-param-checkbox", this.$params).filter(".fr-paramname-" + param.name).first();
+            var me = this;
             var $element = $(".fr-paramname-" + param.name, this.$params);
 
             //check nullable
-            if ($cb.length !== 0 && $cb.attr("checked") === "checked" && param.value === "") {
+            if (me._isNullChecked(param) && param.value === "") {
                 return null;
             } else if ($element.attr("allowblank") === "true" && param.value === "") {
                 //check allow blank
                 return "";
-            } else if (param.value && param.value !== "") {
-                // If it has a real value, go for it.
-                return param.value;
+            } else if (param.attributes.backendValue) {
+                //Take care of the big dropdown list
+                return param.attributes.backendValue.nodeValue;
             } else {
-                // If it is still "", return null to handle the case where the user has yet touched the parameter.
-                return param.attributes.backendValue ? param.attributes.backendValue.nodeValue : null;
+                //Otherwise handle the case where the parameter has not been touched
+                return param.value !== "" ? param.value : null;
             }
         },
         /**

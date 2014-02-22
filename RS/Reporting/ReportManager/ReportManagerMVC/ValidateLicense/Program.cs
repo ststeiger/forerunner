375aa011092e48e155514a622e75a5e59c86e887
stellar <stellar@forerunnersw.com>
2014-02-21 16:06:11 -0800

    Signed-off-by: stellar <stellar@forerunnersw.com>
    1.  Add ValidateLicense command line tool.

---
+++ RS/Reporting/ReportManager/ReportManagerMVC/ValidateLicense/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForerunnerLicense;
+
+namespace ValidateLicense
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                ClientLicense.Validate(true);
+                Console.WriteLine("Validation succeeded.");
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Validation failed.  " + e.ToString());
+                return 1;
+            }
+        }
+    }
+}

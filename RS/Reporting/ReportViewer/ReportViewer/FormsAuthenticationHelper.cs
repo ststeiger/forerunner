101cad7e7eab2875aa5a1208fe77302d617e44d6
Stella Rowlett <stellar@forerunnersw.com>
2014-03-13 19:24:52 -0700

    Signed-off-by: Stella Rowlett <stellar@forerunnersw.com>
    1.  Fixed the auth issue where the web server is not domain joined.

--- RS/Reporting/ReportViewer/ReportViewer/FormsAuthenticationHelper.cs
+++ RS/Reporting/ReportViewer/ReportViewer/FormsAuthenticationHelper.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Security;
+using System.Net;
+using System.Configuration;
+using Forerunner.SSRS.Viewer;
+using Forerunner;
 
 namespace Forerunner.Security
 {
     public class FormsAuthenticationHelper
     {
+        static private string url = ConfigurationManager.AppSettings["Forerunner.ReportServerWSUrl"];
+        static private int ReportServerTimeout = GetAppSetting("Forerunner.ReportServerTimeout", 100000);
+        static private Forerunner.Config.WebConfigSection webConfigSection = Forerunner.Config.WebConfigSection.GetConfigSection();
+
+        static private int GetAppSetting(string key, int defaultValue)
+        {
+            string value = ConfigurationManager.AppSettings[key];
+            return (value == null) ? defaultValue : int.Parse(value);
+        }
+
         static private HttpCookie FindAuthCookie()
         {
             return HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
         }
 
+        static private string GetFirstUrl()
+        {
+            if (url != null) 
+                return url;
+
+            if (webConfigSection != null)
+            {
+                Forerunner.Config.ConfigElementCollection configElementCollection = webConfigSection.InstanceCollection;
+                foreach (Forerunner.Config.ConfigElement configElement in configElementCollection)
+                {
+                    if (configElement.ReportServerWSUrl != null)
+                        return configElement.ReportServerWSUrl;
+                }
+            }
+            return "";
+        }
         static public bool Login(string userNameAndDomain, string password, int timeout)
         {
             
@@ -37,11 +67,35 @@ namespace Forerunner.Security
 
                 try
                 {
-                    if (true)
+                    bool authenticated = Forerunner.Security.NativeMethods.LogonUser(
+                        userName,
+                        domain,
+                        password,
+                        LOGON32_LOGON_NETWORK,
+                        LOGON32_PROVIDER_DEFAULT, ref token);
+
+                    // This is the code path when we cannot authenticate with the Web Server.
+                    // Have to make a call to RS.
+                    if (!authenticated) 
+                    {
+                        try
+                        {
+                            NetworkCredential networkCredential = new NetworkCredential(userName, password, domain);
+                            ReportViewer reportViewer = new ReportViewer(GetFirstUrl(), ReportServerTimeout);
+                            reportViewer.SetCredentials(networkCredential);
+                            reportViewer.forceGetServerRendering();
+                            authenticated = true;
+                        }
+                        catch(Exception e)
+                        {
+                            authenticated = false;
+                        }
+                    }
+
+                    if (authenticated)
                     {
                         // Write the cookie
                         //FormsAuthentication.SetAuthCookie(model.UserName, false);
-
                         FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
                             1,
                             userNameAndDomain,

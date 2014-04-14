a839e6052853888b25bb19356ee98edca38c230c
Jason Carlson <jasonc@forerunnersw.com>
2014-04-13 17:21:20 -0700

    Add error handling to SSRS render
    Fix 10.3 RPL issues

--- RS/Reporting/ReportViewer/Rendering Extension/JSONRenderingExtension/JSONRenderer.cs
+++ RS/Reporting/ReportViewer/Rendering Extension/JSONRenderingExtension/JSONRenderer.cs
@@ -19,33 +19,55 @@ namespace Forerunner.RenderingExtensions
 
     public class JSONRenderer : IRenderingExtension
     {
-        private IRenderingExtension RPL;
+        private IRenderingExtension RPL = null;
         public string LocalizedName { get { return "ForerunnerJSON"; } }
         static Type rplRendererType = null;
         private Stream RegisteredStream = null;
         private ReportJSONWriter JSON;
         private CreateAndRegisterStream RenderStreams = null;
 
-        public JSONRenderer()
+        private void Init()
         {
             if (rplRendererType == null)
             {
-                Assembly IR = Assembly.Load(new AssemblyName("Microsoft.ReportingServices.RPLRendering, Version=10.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91"));
-                rplRendererType = IR.GetType("Microsoft.ReportingServices.Rendering.RPLRendering.RPLRenderer");
+                Assembly IR;
+                IR = Assembly.Load(new AssemblyName("Microsoft.ReportingServices.RPLRendering, Version=10.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91"));
+
+                if (IR != null)
+                    rplRendererType = IR.GetType("Microsoft.ReportingServices.Rendering.RPLRendering.RPLRenderer");
+                else
+                    Logger.Trace(LogType.Error, "JSONRenderer cannot get type");
+
             }
 
             //Create an instance of type RPLRenderer. 
             //Now, RPLRenderer inherits from IRenderingExtension which is a public interface so cast it.
-            RPL = (IRenderingExtension)rplRendererType.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null).Invoke(null);
-        }
+            if (rplRendererType != null)
+                RPL = (IRenderingExtension)rplRendererType.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null).Invoke(null);
+            else
+                Logger.Trace(LogType.Error, "JSONRenderer cannot get inner Render");
 
+        }
+        private IRenderingExtension GetRPL()
+        {
+            if (RPL == null)
+                Init();
+            if (RPL == null)
+            {
+                ExceptionLogGenerator.LogException("Inner Render null", "Init");
+                throw new Exception("Inner Render null");
+            }
+            return RPL;
+
+        }
         public void GetRenderingResource(CreateAndRegisterStream createAndRegisterStreamCallback, NameValueCollection deviceInfo)
         {
-            RPL.GetRenderingResource(createAndRegisterStreamCallback, deviceInfo);
+            GetRPL().GetRenderingResource(createAndRegisterStreamCallback, deviceInfo);
         }
-        
+
         public bool Render(Microsoft.ReportingServices.OnDemandReportRendering.Report report, NameValueCollection reportServerParameters, NameValueCollection deviceInfo, NameValueCollection clientCapabilities, ref Hashtable renderProperties, CreateAndRegisterStream createAndRegisterStream)
         {
+
             try
             {
                 Logger.Trace(LogType.Info, "JSONRenderer.Render " + report.Name);
@@ -54,7 +76,7 @@ namespace Forerunner.RenderingExtensions
                 StringWriter sw;
 
                 Stream outputStream = createAndRegisterStream(report.Name, "json", Encoding.UTF8, "text/json", true, StreamOper.CreateAndRegister);
-                retval = RPL.Render(report, reportServerParameters, deviceInfo, clientCapabilities, ref renderProperties, new Microsoft.ReportingServices.Interfaces.CreateAndRegisterStream(IntermediateCreateAndRegisterStream));
+                retval = GetRPL().Render(report, reportServerParameters, deviceInfo, clientCapabilities, ref renderProperties, new Microsoft.ReportingServices.Interfaces.CreateAndRegisterStream(IntermediateCreateAndRegisterStream));
 
                 RegisteredStream.Position = 0;
                 JSON = new ReportJSONWriter(RegisteredStream);
@@ -62,7 +84,7 @@ namespace Forerunner.RenderingExtensions
                     sw = JSON.RPLToJSON(int.Parse(renderProperties["TotalPages"].ToString()));
                 else
                     sw = JSON.RPLToJSON(0);
-        
+
                 int bufsiz = 1024 * 1000;
                 char[] c = new char[bufsiz];
                 StringBuilder sb;
@@ -90,13 +112,13 @@ namespace Forerunner.RenderingExtensions
                         break;
                     }
 
-                }           
+                }
                 return retval;
             }
             catch (LicenseException e)
             {
                 ExceptionLogGenerator.LogException(e);
-                return false;                
+                return false;
             }
             catch (Exception e)
             {
@@ -107,12 +129,12 @@ namespace Forerunner.RenderingExtensions
 
         public bool RenderStream(string streamName, Microsoft.ReportingServices.OnDemandReportRendering.Report report, NameValueCollection reportServerParameters, NameValueCollection deviceInfo, NameValueCollection clientCapabilities, ref Hashtable renderProperties, CreateAndRegisterStream createAndRegisterStream)
         {
-            return RPL.RenderStream(streamName, report, reportServerParameters, deviceInfo, clientCapabilities, ref renderProperties, createAndRegisterStream);
+            return GetRPL().RenderStream(streamName, report, reportServerParameters, deviceInfo, clientCapabilities, ref renderProperties, createAndRegisterStream);
         }
         //[StrongNameIdentityPermissionAttribute(SecurityAction.LinkDemand, PublicKey = "0024000004800000940000000602000000240000525341310004000001000100272736ad6e5f9586bac2d531eabc3acc666c2f8ec879fa94f8f7b0327d2ff2ed523448f83c3d5c5dd2dfc7bc99c5286b2c125117bf5cbe242b9d41750732b2bdffe649c6efb8e5526d526fdd130095ecdb7bf210809c6cdad8824faa9ac0310ac3cba2aa0523567b2dfa7fe250b30facbd62d4ec99b94ac47c7d3b28f1f6e4c8")]
         public void SetConfiguration(string configuration)
         {
-            RPL.SetConfiguration(configuration);
+            GetRPL().SetConfiguration(configuration);
         }
 
         Stream IntermediateCreateAndRegisterStream(
@@ -135,10 +157,10 @@ namespace Forerunner.RenderingExtensions
             }
 
             return RenderStreams(name, extension, encoding, mimeType, willSeek, operation);
-            
-        }   
+
+        }
     }
 
-   
+
 
 }

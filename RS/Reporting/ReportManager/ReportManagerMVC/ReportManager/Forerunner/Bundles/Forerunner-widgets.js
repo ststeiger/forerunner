cb56e852080e92f952f6c74ad36432142bb1c310
sdwangbaotong <sdwangbaotong@hotmail.com>
2014-05-06 20:27:29 +0800

    Fixed issue 1016, add textbox focus/blur event handler for iOS in manage set widget

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -10228,6 +10228,18 @@ $(function () {
                 me._onClickDefault(e);
             });
 
+            $(":text", me.element).each(
+               function (index) {
+                   var textinput = $(this);
+                   textinput.on("blur", function () {
+                       me.options.$reportViewer.reportViewer("onInputBlur");
+                   });
+                   textinput.on("focus", function () {
+                       me.options.$reportViewer.reportViewer("onInputFocus");
+                   });
+               }
+           );
+
             // Set up the form validation
             me._validateForm(me.$form);
         },

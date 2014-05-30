4e8471ae3cf8785517230b8a0f530d0838876bbe
Jason Carlson <jasonc@forerunnersw.com>
2014-05-30 08:06:33 -0700

    fix _endsWith

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner.js
@@ -493,7 +493,7 @@ $(function () {
         * @param {String} Forerunner folder path.
         */
         setForerunnerFolder: function (forerunnerFolderPath) {
-            if (_endsWith(forerunnerFolderPath, "/") === -1) {
+            if (this._endsWith(forerunnerFolderPath, "/") === -1) {
                 this._forerunnerFolderPath = forerunnerFolderPath + "/";
             } else {
                 this._forerunnerFolderPath = forerunnerFolderPath;
@@ -514,7 +514,7 @@ $(function () {
         * @param {String} API Base.
         */
         setAPIBase: function (apiBase) {
-            if (_endsWith(apiBase, "/") === -1) {
+            if (this._endsWith(apiBase, "/") === -1) {
                 this._apiBase = apiBase + "/";
             } else {
                 this._apiBase = apiBase;
--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Common/js/forerunner.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Common/js/forerunner.js
@@ -492,7 +492,7 @@ $(function () {
         * @param {String} Forerunner folder path.
         */
         setForerunnerFolder: function (forerunnerFolderPath) {
-            if (_endsWith(forerunnerFolderPath, "/") === -1) {
+            if (this._endsWith(forerunnerFolderPath, "/") === -1) {
                 this._forerunnerFolderPath = forerunnerFolderPath + "/";
             } else {
                 this._forerunnerFolderPath = forerunnerFolderPath;
@@ -513,7 +513,7 @@ $(function () {
         * @param {String} API Base.
         */
         setAPIBase: function (apiBase) {
-            if (_endsWith(apiBase, "/") === -1) {
+            if (this._endsWith(apiBase, "/") === -1) {
                 this._apiBase = apiBase + "/";
             } else {
                 this._apiBase = apiBase;

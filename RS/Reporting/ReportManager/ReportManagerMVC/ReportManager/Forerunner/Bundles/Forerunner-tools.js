3d62a03171e993362aec8f633e9746fb46bb6a83
sdwangbaotong <sdwangbaotong@hotmail.com>
2014-07-24 12:16:12 +0800

    Fixed issue 1202, the selector of keyword textbox definition is wrong.

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-tools.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-tools.js
@@ -1028,8 +1028,8 @@ $(function () {
         /** @member */
         itemKeyword: {
             toolType: toolTypes.input,
-            selectorClass: "fr-item-keyword-textbox fr-core-input",
-            sharedClass: "fr-toolbase-find-textbox fr-item-keyword-textbox",
+            selectorClass: "fr-item-keyword-textbox",
+            sharedClass: "fr-toolbase-find-textbox fr-core-input",
             tooltip: locData.toolbar.keyword,
             events: {
                 keydown: function (e) {

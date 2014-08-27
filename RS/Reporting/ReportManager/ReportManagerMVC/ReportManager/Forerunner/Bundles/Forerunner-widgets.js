b415f013e22a184d2309dba1a6a255bf2b75e157
Jason Carlson <jasonc@forerunnersw.com>
2014-08-27 07:18:32 -0700

    add support for custom watermark

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -8852,8 +8852,11 @@ $(function () {
         _getWatermark: function () {
 
             var wstyle = "opacity:0.30;color: #d0d0d0;font-size: 120pt;position: absolute;margin: 0;left:0px;top:40px; pointer-events: none;";
+
+            var postText = forerunner.config.getCustomSettingsValue("WatermarkPostText", "");
+
             if (forerunner.device.isMSIE8() || forerunner.device.isAndroid()) {
-                var wtr = $("<DIV/>").html("Evaluation");
+                var wtr = $("<DIV/>").html("Evaluation </br></br>" + postText);
                 wstyle += "z-index: -1;";
                 wtr.attr("style", wstyle);
                 return wtr;
@@ -8880,7 +8883,15 @@ $(function () {
             text.setAttribute("y", "160");
             text.setAttribute("fill", "#000");
             text.setAttribute("pointer-events", "none");
-            text.textContent = "E" + "val" + "ua" + "tion";
+
+            var span = document.createElementNS("http://www.w3.org/2000/svg", "tspan");            
+            span.textContent = "Evaluation";
+            text.appendChild(span);
+            var span = document.createElementNS("http://www.w3.org/2000/svg", "tspan");
+            span.setAttribute("x", "10");
+            span.setAttribute("y", "280");
+            span.textContent = postText;
+            text.appendChild(span);
 
             svg.appendChild(text);
 

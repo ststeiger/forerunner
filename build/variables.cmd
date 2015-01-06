74b5ef87c3a22fdb4a52aa2cef25a92227899c3e
Stella Rowlett <stellacello@gmail.com>
2015-01-05 22:00:12 -0800

    Undo Erroneous merge of v3 into master.

--- build/variables.cmd
+++ build/variables.cmd
@@ -4,8 +4,8 @@ REM Set Project specific variables here.
 REM
 set PATH=%PATH%;%windir%\Microsoft.Net\Framework\v4.0.30319
 set EnableNuGetPackageRestore=true
-set BRANCH=v3
-set GITHUBSSH=origin v3
+set BRANCH=master
+set GITHUBSSH=git@github.com:forerunnersw/Forerunner.git
 set PROJECT_NAME=Forerunner
 set HOME=%HOMEDRIVE%%HOMEPATH%
 set SPSITE=https://forerunnersw.sharepoint.com

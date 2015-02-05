32c3ee45efd0eb5e23c8e92865d350022bc5bf85
Jason Carlson <jasonc@forerunnersw.com>
2015-02-04 19:08:11 -0600

    update website for pass ads
    Add support for subscription input limits

--- RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
+++ RS/Reporting/ReportManager/ReportManagerMVC/ReportManager/Forerunner/Bundles/Forerunner-widgets.js
@@ -18549,10 +18549,13 @@ $(function () {
             me.$theTable.addClass("fr-email-table");
             me.$theForm.append(me.$theTable);
             me.$desc = me._createInputWithPlaceHolder(["fr-email-description"], "text", locData.subscription.descriptionPlaceholder);
+            me.$desc.attr("maxlength", forerunner.config.getCustomSettingsValue("SubscriptionInputSize", "100"));
             me.$theTable.append(me._createTableRow(locData.subscription.descriptionPlaceholder, me.$desc));
             me.$to = me._createInputWithPlaceHolder(["fr-email-to"], "text", locData.subscription.toPlaceholder);
+            me.$to.attr("maxlength", forerunner.config.getCustomSettingsValue("SubscriptionInputSize", "100"));
             me.$theTable.append(me._createTableRow(locData.subscription.toPlaceholder, me.$to));
             me.$subject = me._createInputWithPlaceHolder(["fr-email-subject"], "text", locData.subscription.subjectPlaceholder);
+            me.$subject.attr("maxlength", forerunner.config.getCustomSettingsValue("SubscriptionInputSize", "100"));
             me.$theTable.append(me._createTableRow(locData.subscription.subjectPlaceholder, me.$subject));
             me.$includeLink = me._createCheckBox();
             me.$includeLink.addClass("fr-email-include");
@@ -18736,7 +18739,7 @@ $(function () {
             var me = this;
             var $listItem = new $("<DIV />");
             $listItem.addClass("fr-sub-listitem");
-            $listItem.append(subInfo.Description);
+            $listItem.append( me._createDiv(["fr-sub-descr"]).text(subInfo.Description));
             var $deleteIcon = me._createDiv(["fr-sub-icon18x18"]);
             var $editIcon = me._createDiv(["fr-sub-icon18x18"]);
             $listItem.append($deleteIcon);

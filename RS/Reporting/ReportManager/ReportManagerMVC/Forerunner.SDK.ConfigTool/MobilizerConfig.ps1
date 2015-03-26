26b2af0116253548301eb3b92d8b306b7bc2bb73
jont <jont@forerunnersw.com>
2015-03-26 01:18:20 -0700

    - Added MobilizerConfig.ps1
    - Signed off by Jon Totton

---
+++ RS/Reporting/ReportManager/ReportManagerMVC/Forerunner.SDK.ConfigTool/MobilizerConfig.ps1
@@ -0,0 +1,15 @@
+# Usage
+#
+# Import-Module MobilizerConfig.ps1
+# Set-FRConfig -WebConfigPath $mobilizerWebConfig
+
+
+$mobilizerConfig = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+$mobilizerInstalled = (Join-Path -Path $mobilizerConfig -ChildPath "..\")
+$mobilizerWebConfig = (Join-Path -Path $mobilizerInstalled -ChildPath "web.config")
+$mobilizerLoaded = Get-Module Forerunner.SDK.ConfigTool.dll
+
+if (!$mobilizerLoaded)
+{
+        Import-Module (Join-Path $scriptDirectory Forerunner.SDK.ConfigTool.dll)
+}

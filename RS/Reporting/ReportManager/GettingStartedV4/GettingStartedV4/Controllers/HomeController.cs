c17a40f9279657361f9ca107f1a6f63db3b6c5ae
jont <jont@forerunnersw.com>
2015-03-28 01:10:42 -0700

    - Initial push of the GettingStartedV4 solution
    - Signed off by Jon Totton

---
+++ RS/Reporting/ReportManager/GettingStartedV4/GettingStartedV4/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GettingStartedV4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult GettingStartedV4()
+        {
+            return View();
+        }
+
+    }
+}

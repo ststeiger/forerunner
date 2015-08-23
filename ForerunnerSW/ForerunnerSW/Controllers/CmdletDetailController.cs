aa64ba608bac40b141cd41623699f0ba4a8fa388
jont <jont@forerunnersw.com>
2015-08-23 13:18:43 -0700

    - Added the references and documentation pages for the PowerShell cmdlets
    - Signed off by Jon Totton

---
+++ ForerunnerSW/ForerunnerSW/Controllers/CmdletDetailController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForerunnerSW.Controllers
+{
+    public class CmdletDetailController : Controller
+    {
+        //
+        // GET: /SampleDetail/
+
+        public ActionResult Summary()
+        {
+            return View();
+        }
+        public ActionResult PublishForerunnerRDLExt()
+        {
+            return View();
+        }
+        public ActionResult PublishFRExtension()
+        {
+            return View();
+        }
+        public ActionResult AddFRScriptRef()
+        {
+            return View();
+        }
+        public ActionResult SetFRConfig()
+        {
+            return View();
+        }
+    }
+}
